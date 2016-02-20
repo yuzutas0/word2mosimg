acd0386ef9e64bc139a28ba082131f0e09be5de7
yuzutas0 <sho.y0921@gmail.com>
2016-02-20 13:34:05 +0900

    code formatter by rubocop

--- main.rb
+++ main.rb
@@ -1,23 +1,21 @@
 class Main
+  # TODO: input keyword
+  # todo validate
 
-  # todo input keyword
-    # todo validate
+  # TODO: input goal_image
+  # todo validate
+  # todo convert to specific size and color
 
-  # todo input goal_image
-    # todo validate
-    # todo convert to specific size and color
+  # TODO: search images by keyword
+  # todo convert to specific size and color
 
-  # todo search images by keyword
-    # todo convert to specific size and color
+  # TODO: learn combination of images
+  # todo convert goal_image to vector
+  # todo convert images to vector(element)
+  # todo convert each element to mean color
+  # todo sort each element (or least squares method)
 
-  # todo learn combination of images
-    # todo convert goal_image to vector
-    # todo convert images to vector(element)
-    # todo convert each element to mean color
-    # todo sort each element (or least squares method)
-
-  # todo make image
-    # todo connect images to one bigger image with sort result
-    # todo export image file
-
+  # TODO: make image
+  # todo connect images to one bigger image with sort result
+  # todo export image file
 end
