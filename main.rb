d29591796e7a971d208a73f77b0ca3be76c9197f
yuzutas0 <sho.y0921@gmail.com>
2016-02-20 14:20:01 +0900

    fix position of comment

--- main.rb
+++ main.rb
@@ -1,29 +1,39 @@
+# main class
 class Main
-  # TODO: input keyword
-  # todo validate
-
-  # TODO: input goal_image
-  # todo validate
-  # todo convert to specific size and color
-
-  # TODO: search images by keyword
-  # todo convert to specific size and color
-
-  # TODO: learn combination of images
-  # todo convert goal_image to vector
-  # todo convert images to vector(element)
-  # todo convert each element to mean color
-  # todo sort each element (or least squares method)
-
-  # TODO: make image
-  # todo connect images to one bigger image with sort result
-  # todo export image file
-
   def init
     # TODO: manage params
   end
 end
 
-# TODO: start action
+# ----------------------------------------
+# parameter
+# ----------------------------------------
+
+# TODO: set parameter
+
+# ----------------------------------------
+# main action
+# ----------------------------------------
+
 main = Main.new
 main.init
+
+# TODO: input keyword
+# todo validate
+
+# TODO: input goal_image
+# todo validate
+# todo convert to specific size and color
+
+# TODO: search images by keyword
+# todo convert to specific size and color
+
+# TODO: learn combination of images
+# todo convert goal_image to vector
+# todo convert images to vector(element)
+# todo convert each element to mean color
+# todo sort each element (or least squares method)
+
+# TODO: make image
+# todo connect images to one bigger image with sort result
+# todo export image file
