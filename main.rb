49abe364be76acfe2c4a2b2e11b7096437b12a70
yuzutas0 <sho.y0921@gmail.com>
2016-02-20 12:59:59 +0900

    make main script file

---
+++ main.rb
@@ -0,0 +1,23 @@
+class Main
+
+  # todo input keyword
+    # todo validate
+
+  # todo input goal_image
+    # todo validate
+    # todo convert to specific size and color
+
+  # todo search images by keyword
+    # todo convert to specific size and color
+
+  # todo learn combination of images
+    # todo convert goal_image to vector
+    # todo convert images to vector(element)
+    # todo convert each element to mean color
+    # todo sort each element (or least squares method)
+
+  # todo make image
+    # todo connect images to one bigger image with sort result
+    # todo export image file
+
+end
