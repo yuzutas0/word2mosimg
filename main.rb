34bf2cc3f2048dc344d7e7d06f72f07daca1ebb3
yuzutas0 <sho.y0921@gmail.com>
2016-02-21 19:05:42 +0900

    delegate execution to class

--- main.rb
+++ main.rb
@@ -1,39 +1,52 @@
 # main class
 class Main
+  # ----------------------------------------
+  # library
+  # ----------------------------------------
+
+  # TODO: set libaries
+  # todo require 'open-uri'
+  # todo require 'kconv'
+  # todo require 'json'
+
+  # ----------------------------------------
+  # parameter
+  # ----------------------------------------
+
   def init
-    # TODO: manage params
+    @keyword = 'test'
+  end
+
+  # ----------------------------------------
+  # main action
+  # ----------------------------------------
+
+  def execute
+    puts @keyword
+
+    # TODO: input keyword
+    # todo validate
+
+    # TODO: input goal_image
+    # todo validate
+    # todo convert to specific size and color
+
+    # TODO: search images by keyword
+    # todo convert to specific size and color
+
+    # TODO: learn combination of images
+    # todo convert goal_image to vector
+    # todo convert images to vector(element)
+    # todo convert each element to mean color
+    # todo sort each element (or least squares method)
+
+    # TODO: make image
+    # todo connect images to one bigger image with sort result
+    # todo export image file
   end
 end
 
-# ----------------------------------------
-# parameter
-# ----------------------------------------
-
-# TODO: set parameter
-
-# ----------------------------------------
-# main action
-# ----------------------------------------
-
+# initialize and execute
 main = Main.new
 main.init
-
-# TODO: input keyword
-# todo validate
-
-# TODO: input goal_image
-# todo validate
-# todo convert to specific size and color
-
-# TODO: search images by keyword
-# todo convert to specific size and color
-
-# TODO: learn combination of images
-# todo convert goal_image to vector
-# todo convert images to vector(element)
-# todo convert each element to mean color
-# todo sort each element (or least squares method)
-
-# TODO: make image
-# todo connect images to one bigger image with sort result
-# todo export image file
+main.execute
