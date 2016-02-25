e4ecb6aa78b3b361a640ce6714ecf4f3e8281fdc
yuzutas0 <sho.y0921@gmail.com>
2016-02-25 23:26:08 +0900

    add combiner class

---
+++ scripts/combiner.rb
@@ -0,0 +1,36 @@
+# combiner class
+class Combiner
+  # ----------------------------------------
+  # library
+  # ----------------------------------------
+
+  # as necessary
+
+  # ----------------------------------------
+  # parameter
+  # ----------------------------------------
+
+  # common
+  ASSETS_PATH = (Dir.pwd + File::SEPARATOR + 'assets' + File::SEPARATOR).freeze
+  TEXT_FILE_SUFFIX = '.txt'.freeze
+  EXPORT_LIST_SEPARATOR = ','.freeze
+
+  # devided
+  DIVIDED_PATH = (ASSETS_PATH + 'divideds' + File::SEPARATOR).freeze
+
+  # combination
+  COMBINED_PATH = (ASSETS_PATH + 'combinations' + File::SEPARATOR).freeze
+  COMBINED_FILE_PREFIX = 'line'.freeze
+
+  def init
+    self
+  end
+
+  # ----------------------------------------
+  # main action
+  # ----------------------------------------
+
+  # ----------------------------------------
+  # helper methods - xxx
+  # ----------------------------------------
+end
