5b2c21094fd26dc71a9aeab1a9d7c7624ba60fe7
yuzutas0 <sho.y0921@gmail.com>
2016-03-06 22:59:11 +0900

    main use list of some keywords

--- main.rb
+++ main.rb
@@ -15,8 +15,8 @@ class Main
   # parameter
   # ----------------------------------------
 
-  def init(keyword)
-    @keyword = keyword
+  def init(keywords)
+    @keywords = keywords
     self
   end
 
@@ -25,34 +25,37 @@ class Main
   # ----------------------------------------
 
   def execute
-    # reductor = Reductor.new.init
-    #
-    # # target file into 200 * 200 gray scaled
-    # reductor.target
-    #
-    # # search images by keyword
-    # Crawler.new.init.crawl_images @keyword
-    #
-    # # original images into 100 * 100 gray scaled
-    # reductor.element
-    #
-    # # element images into 1 * 1 to learn
-    # reductor.pixel
-    #
-    # # analyze color diff of target and pixel files
-    # Analyzer.new.init.diff_colors
+    input_images(@keywords)
+    output_images
+  end
 
+  # crawl and reduce images
+  def input_images(keywords)
+    reductor = Reductor.new.init
+    # target file into 200 * 200 gray scaled
+    reductor.target
+    # search images by keyword
+    keywords.each { |keyword| Crawler.new.init.crawl_images keyword }
+    # original images into 100 * 100 gray scaled
+    reductor.element
+    # element images into 1 * 1 to learn
+    reductor.pixel
+  end
+
+  # learn and combine, export mosaic image
+  def output_images
+    # analyze color diff of target and pixel files
+    Analyzer.new.init.diff_colors
     # divide target and pixel colors to 8 pattern
-    # Divider.new.init.divide
-
+    Divider.new.init.divide
     # combine target colors and element files
-    # Combiner.new.init.combine
-
+    Combiner.new.init.combine
+    # draw mosaic image - simple connect / resize
     Drawer.new.init.draw
   end
 end
 
 # initialize and execute
 require 'uri'
-keyword = URI.escape '愛'
-Main.new.init(keyword).execute
+keywords = [URI.escape('愛'), URI.escape('love')]
+Main.new.init(keywords).execute
