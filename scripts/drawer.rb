8b1bc74b7d4e13296c0a0b155c4d39f9e7c2a824
yuzutas0 <sho.y0921@gmail.com>
2016-03-06 22:01:58 +0900

    drawer export mosaic image

--- scripts/drawer.rb
+++ scripts/drawer.rb
@@ -13,6 +13,7 @@ class Drawer
   # common
   ASSETS_PATH = (Dir.pwd + File::SEPARATOR + 'assets' + File::SEPARATOR).freeze
   JPG_FILE_SUFFIX = '.jpg'.freeze
+  PNG_FILE_SUFFIX = '.png'.freeze
   TEXT_FILE_SUFFIX = '.txt'.freeze
 
   # combination
@@ -24,11 +25,10 @@ class Drawer
   ELEMENTS_PATH = (ASSETS_PATH + 'elements' + File::SEPARATOR).freeze
 
   # draw
-  DRAW_PATH = (ASSETS_PATH + 'draws').freeze
+  DRAW_PATH = (ASSETS_PATH + 'draws' + File::SEPARATOR).freeze
   SIDE_PICTURES_COUNT = 200
-  # TODO: line file is line_001.jpg - line_200.jpg
   DRAW_LINE_FILE_NAME_PREFIX = (DRAW_PATH + 'line_').freeze
-  DRAW_MOSAIC_FILE_NAME = (DRAW_PATH + 'mosaic' + JPG_FILE_SUFFIX).freeze
+  DRAW_MOSAIC_FILE_NAME = (DRAW_PATH + 'mosaic' + PNG_FILE_SUFFIX).freeze
 
   def init
     self
@@ -39,14 +39,11 @@ class Drawer
   # ----------------------------------------
 
   def draw
-    image_name_list = get_image_name_list COMBINATION_FILE_NAME
-    (0..SIDE_PICTURES_COUNT - 1).each do |index|
-      image_name_list_for_line = get_list_for_line(index, image_name_list)
-      puts image_name_list_for_line
-    end
-    # TODO: connect images for the line
-    # TODO: export connected image to draws
-    # TODO: connect each line images after loop
+    # image_name_list = get_image_name_list COMBINATION_FILE_NAME
+    # (0..SIDE_PICTURES_COUNT - 1).each do |index|
+    #   make_image_for_line(index, image_name_list)
+    # end
+    make_mosaic_image
   end
 
   # ----------------------------------------
@@ -60,11 +57,45 @@ class Drawer
     image_name_list
   end
 
+  # export image about the line
+  def make_image_for_line(index, image_name_list)
+    image_name_list_for_line = get_list_for_line(index, image_name_list)
+    line_image = Magick::ImageList.new
+    image_name_list_for_line.each do |image_name|
+      pixel_image = Magick::ImageList.new(ELEMENTS_PATH + image_name)
+      line_image << pixel_image.append(false)
+      pixel_image.destroy!
+    end
+    image_name = get_image_name(index)
+    line_image.append(false).write(image_name)
+    line_image.destroy!
+  end
+
   # choose image only about the line
   def get_list_for_line(index, image_name_list)
     first_image_index = index * SIDE_PICTURES_COUNT
     last_image_index = first_image_index + SIDE_PICTURES_COUNT - 1
     list_for_line = image_name_list[first_image_index..last_image_index]
     list_for_line
+  end
+
+  # get image name about the line
+  def get_image_name(index)
+    index_string = format('%03d', index)
+    image_name = DRAW_LINE_FILE_NAME_PREFIX + index_string + JPG_FILE_SUFFIX
+    image_name
+  end
+
+  # export mosaic image based on each line images
+  def make_mosaic_image
+    mosaic_image = Magick::ImageList.new
+    (0..SIDE_PICTURES_COUNT - 1).each do |index|
+      line_image_name = get_image_name(index)
+      line_image = Magick::ImageList.new(line_image_name)
+      mosaic_image << line_image.append(true)
+      line_image.destroy!
+    end
+    mosaic_image.append(true).write(DRAW_MOSAIC_FILE_NAME)
+    mosaic_image.destroy!
   end
 end
